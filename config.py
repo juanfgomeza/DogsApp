1ed6c0cd4a695aa82e96dc07adaa9a38e7041b26
AzureAD\JuanFernandoGomez <juanfgomeza@gmail.com>
2022-09-21 10:54:49 -0500

    config vars heroku v1

--- config.py
+++ config.py
@@ -15,5 +15,11 @@ class DevelopmentConfig(Config):
 
 class ProductionConfig(Config):
     ENV = 'production'
-    SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://b38e624b36920a:4091778d@us-cdbr-east-06.cleardb.net/heroku_f9d43f2f20fae70'
+    dbUser = os.environ.get('DB_USER')
+    dbPassword = os.environ.get('DB_PASSWORD')
+    dbServer = os.environ.get('DB_SERVER')
+    dbDatabase = os.environ.get('DB_DATABASE')
+    SQLALCHEMY_DATABASE_URI = f'mysql+pymysql://{dbUser}:{dbPassword}@{dbServer}/{dbDatabase}'    
+    #SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://b38e624b36920a:4091778d@us-cdbr-east-06.cleardb.net/heroku_f9d43f2f20fae70'    
+    #How to make it reconnect?
     
